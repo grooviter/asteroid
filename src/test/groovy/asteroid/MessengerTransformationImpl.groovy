4126b00487ffc5cdae0f60a2b5c05e2f468b27ec
Janus Lynd <januslynd@gmail.com>
2016-01-24 18:57:15 +0100

    Add checkTrue/checkFalse

--- src/test/groovy/asteroid/MessengerTransformationImpl.groovy
+++ src/test/groovy/asteroid/MessengerTransformationImpl.groovy
@@ -18,11 +18,19 @@ import org.codehaus.groovy.control.SourceUnit
 @LocalTransformation(A.PHASE_LOCAL.INSTRUCTION_SELECTION)
 class MessengerTransformationImpl extends LocalTransformationImpl<Messenger,MethodNode> {
 
+    private static final String MESSAGE_ERR_STRANGE = "strange() method should not exist"
+    private static final String MESSAGE_ERR_ANN_TYPE = "Annotation should be of type Messenger"
+
     @Override
     void doVisit(final AnnotationNode annotation, final MethodNode method, final SourceUnit sourceUnit) {
+        Boolean badMethodIsThere   = method.declaringClass.methods.any { it.name == 'badMethod'}
+        Boolean annotationIsOfType = A.UTIL.isOrImplements(annotation.classNode, Messenger)
+
         CHECK.source(sourceUnit) {
             checkThat method, hasName("saySomething")
             checkThat annotation, isAppliedOnceTo(method.declaringClass.methods)
+            checkTrue annotation, annotationIsOfType, MESSAGE_ERR_ANN_TYPE
+            checkFalse annotation, badMethodIsThere, MESSAGE_ERR_STRANGE
         }
 
         method.code = STMT.returnS(EXPR.constX(UTIL.getStringValue(annotation)));
