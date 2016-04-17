4542c394ad1237c278d181394962196ff78b76c6
mariogarcia <mario.ggar@gmail.com>
2016-04-17 19:30:05 +0200

    Update documentation

--- asteroid-test/src/main/groovy/asteroid/utils/AddImportTransformer.groovy
+++ asteroid-test/src/main/groovy/asteroid/utils/AddImportTransformer.groovy
@@ -11,17 +11,18 @@ import asteroid.global.ClassNodeTransformer
  *
  * @since 0.1.6
  */
-class AddImportTransformer extends ClassNodeTransformer {
-
+// tag::classnodetransformer[]
+class AddImportTransformer extends ClassNodeTransformer { // <1>
     public AddImportTransformer(final SourceUnit sourceUnit) {
-        super(sourceUnit, byAnnotationName(AddImport.simpleName))
+        super(sourceUnit, byAnnotationName(AddImport.simpleName)) // <2>
     }
 
     /**
      * {@inheritDocs}
      */
     @Override
-    void transformClass(final ClassNode target) {
-        A.UTIL.CLASS.addImport(target, groovy.json.JsonOutput)
+    void transformClass(final ClassNode target) { // <3>
+        A.UTIL.CLASS.addImport(target, groovy.json.JsonOutput) // <4>
     }
 }
+// end::classnodetransformer[]
