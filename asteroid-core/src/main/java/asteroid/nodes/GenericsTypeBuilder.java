6050ee8beeaa26fe8b28d77528b09e94a4e0de64
Mario Garcia <mario.ggar@gmail.com>
2019-02-02 00:25:00 +0100

    Add javadoc completion to GenericsTypeBuilder

--- asteroid-core/src/main/java/asteroid/nodes/GenericsTypeBuilder.java
+++ asteroid-core/src/main/java/asteroid/nodes/GenericsTypeBuilder.java
@@ -12,20 +12,32 @@ final public class GenericsTypeBuilder {
 
     private final GenericsType genericsType;
 
+    /**
+     * Sets the type of the generic type
+     *
+     * @param classNode Sets the {@link ClassNode} of the generic type
+     * @since 0.1.0
+     */
     private GenericsTypeBuilder(final ClassNode classNode) {
         this.genericsType = new GenericsType(classNode);
     }
 
     /**
-     * @param classNode
+     * Sets the type of the generic type
+     *
+     * @param classNode Sets the {@link ClassNode} of the generic type
      * @return an instance of type {@link GenericsTypeBuilder}
+     * @since 0.1.0
      */
     public static GenericsTypeBuilder generics(final ClassNode classNode) {
         return new GenericsTypeBuilder(classNode);
     }
 
     /**
+     * Returns the configured generic type
+     *
      * @return an instance of type {@link GenericsType}
+     * @since 0.1.0
      */
     public GenericsType build() {
         return this.genericsType;
