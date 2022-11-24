5861f14f4c8d73e6970dc4ce78e80622d324c28f
Mario Garcia <mario.ggar@gmail.com>
2022-11-25 00:01:53 +0100

    Publish guide for 0.5.0

---
+++ api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"asteroid"},{"l":"asteroid.internal"},{"l":"asteroid.nodes"},{"l":"asteroid.statements"},{"l":"asteroid.transformer"},{"l":"asteroid.utils"}]
